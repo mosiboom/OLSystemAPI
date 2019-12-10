c9a9ab12d16f55e1b0da62439082cb6d606ba40d
Jasper <936022546@qq.com>
2019-12-11 00:38:34 +0800

    缓存获取文章详情路由

--- application/index/controller/Article.php
+++ application/index/controller/Article.php
@@ -3,6 +3,7 @@
 namespace app\index\controller;
 
 use app\server\SerPublic;
+use think\cache\driver\Redis;
 use think\Controller;
 use think\Db;
 use think\db\exception\DataNotFoundException;
@@ -47,15 +48,23 @@ class Article extends Controller
             if (!$id) {
                 throw new \RuntimeException('参数有误');
             }
+            $redis = new Redis();
+            $info = $redis->get("ol_system_article_{$id}");
+            if ($info) {
+                return SerPublic::ApiJson(json_decode($info, true), 0, 'success');
+            }
             $info = Db::table('article')
                 ->where('id', $id)
                 ->field('*')
                 ->find();
+
             if (!$info) {
                 throw new DataNotFoundException('数据不存在！');
             }
             $info['create_time'] = date('Y-m-d H:i', $info['create_time']);
             $info['update_time'] = date('Y-m-d H:i', $info['update_time']);
+            $redis->set("ol_system_article_{$id}", json_encode($info), '3600');
+
             return SerPublic::ApiJson($info, 0, 'success');
         } catch (\RuntimeException $exception) {
             return SerPublic::ApiJson('', 101, $exception->getMessage());
