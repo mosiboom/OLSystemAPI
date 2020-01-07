c32defe0181a5e12fec69d734c95baa54a92c2b3
Jasper <936022546@qq.com>
2020-01-08 00:11:33 +0800

    test update one file

---
+++ application/index/controller/Section.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\index\controller;
+
+use think\{Controller,
+    Db,
+    db\exception\DataNotFoundException,
+    db\exception\ModelNotFoundException,
+    exception\DbException,
+    facade\Request
+};
+
+class Section extends Controller
+{
+    public function getAll()
+    {
+    }
+
+    public function getOne()
+    {
+    }
+
+    public function delete()
+    {
+    }
+
+    public function insert()
+    {
+    }
+
+    public function save()
+    {
+    }
+
+}
---
+++ route/section.php
@@ -0,0 +1,9 @@
+<?php
+/*课程小节相关接口*/
+
+use think\facade\{Request, Route};
+
+
+Route::get('/get', function (){
+    echo 1;
+});
