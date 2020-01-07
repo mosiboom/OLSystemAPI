50e7ea39fb3bcc94a9d7724518449e16fc8a92d8
Jasper <936022546@qq.com>
2020-01-07 18:04:14 +0800

    change route file

--- route/route.php
+++ route/route.php
@@ -8,24 +8,3 @@ Route::get('/index', 'index/index');
 Route::post('/login', 'login/index');
 Route::post('/logout', 'login/logout');
 Route::post('/token/refresh', 'login/refresh');
-
-//Route::get('/mockComment', 'article/mock');
-Route::get('/loginTest', function () {
-    $open_id = Request::param('payload')['uid'];
-    echo "用户已登录，这是用户的id：" . $open_id;
-})->middleware('auth');
-Route::get('/test', function () {
-    $tokenArr = \app\server\SerAuth::makeToken('213123');
-    dump($tokenArr['access_token']);
-    // dump(md5($tokenArr['refresh_token']));
-    //dump(rand(100, 9999));
-});
-
-Route::get('/test1', function () {
-    dump(Env::get('app_path'));
-    dump(config('exception_tmpl'));
-});
-
-/*Route::get('/script/run', 'script/run');
-Route::get('/getJueJin', 'index/getJueJinArticle');
-Route::get('/getImooc', 'index/getImoocClass');*/
